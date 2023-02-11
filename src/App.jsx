0de4f11d42e28853745d880c3669dfb6eb683887
magna <julijagarkusha@gmail.com>
2023-02-11 11:13:02 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import './App.scss';
+
+function App() {
+
+  useEffect(() => {
+    document.title = 'Domashka29';
+  }, []);
+
+  return (
+    <main className="app">
+
+    </main>
+  );
+}
+
+export default App;
