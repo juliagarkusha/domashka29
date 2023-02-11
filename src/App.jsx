4c49fac36b1ec74a32f3c245014a5a2be07dac8f
magna <julijagarkusha@gmail.com>
2023-02-11 20:46:00 +0200

    added domashka step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import Name from "./components/Name";
+import FavoriteAnimal from "./components/FavoriteAnimal";
+import Display from "./components/ Display";
 import './App.scss';
 
 function App() {
+  const [name, setName] = useState('');
+  const [animal, setAnimal] = useState('');
 
   useEffect(() => {
     document.title = 'Domashka29';
   }, []);
 
   return (
-    <main className="app">
-
-    </main>
+    <form>
+      <Name
+        name={name}
+        onNameChange={event => setName(event.target.value)}
+      />
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={event => setAnimal(event.target.value)}
+      />
+      <Display
+        name={name}
+        animal={animal}
+      />
+    </form>
   );
 }
 
